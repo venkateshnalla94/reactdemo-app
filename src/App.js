676d0a3a6fccf7bf2a0abf29447d1d0ebfc78f77
Venkatesh Nalla <venkateshnalla@Venkateshs-MacBook-Air.local>
2019-05-23 18:19:12 -0400

    added few intial prject files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Welcome_View from './Component/Login_page/Welcome_View';
+class App extends Component{
+  render() {
+    return (
+      <div>
+       <Welcome_View></Welcome_View>
+      </div>
+    )
+
+  }
 }
-
 export default App;

6ac6c4b122b8edc9f5cece7ac2271d163d50cbb3
venkateshnalla94 <venkateshnalla94@gmail.com>
2019-08-28 17:29:40 -0400

    Navbar

--- src/components/NavigationDashboard.js
+++ src/components/NavigationDashboard.js
@@ -3,11 +3,24 @@ import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 // import Navbar.Brand from "react-bootstrap/NavbarBrand";
 import "../pages/landing/landing_css/navigationDashboard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendar,
+  faUser,
+  faTable,
+  faClipboardCheck,
+  faDrumstickBite,
+  faMapMarkerAlt,
+  faNewspaper,
+  faBell,
+  faSnowboarding,
+  faChevronCircleUp
+} from "@fortawesome/free-solid-svg-icons";
 const NavigationDashboard = () => {
   return (
     <div>
       <div className="col-sm-2" id="layer">
-        <div>
+        <div className="container-fluid" id="white_smoke">
           <h4 className="d-flex justify-content-center">
             <span id="sideBar_heading">Creative Tim</span>
           </h4>
@@ -15,47 +28,86 @@ const NavigationDashboard = () => {
 
           <Navbar id="nav_bar">
             <Navbar.Brand href="#home" id="nav_bar" block>
-              Dashboard
+              <FontAwesomeIcon
+                icon={faCalendar}
+                style={{ cursor: "pointer" }}
+              />
+              &nbsp;&nbsp;&nbsp;
+              <h6 id="side_bar_headings">Dashboard</h6>
             </Navbar.Brand>
           </Navbar>
           <Navbar id="nav_bar">
             <Navbar.Brand href="#home" id="nav_bar">
-              User Profile
+              <FontAwesomeIcon icon={faUser} style={{ cursor: "pointer" }} />
+              &nbsp;&nbsp;&nbsp;
+              <h6 id="side_bar_headings">User Profile</h6>
             </Navbar.Brand>
           </Navbar>
           <Navbar id="nav_bar">
             <Navbar.Brand href="#home" id="nav_bar">
-              Table List
+              <FontAwesomeIcon
+                icon={faClipboardCheck}
+                style={{ cursor: "pointer" }}
+              />
+              &nbsp;&nbsp;&nbsp;
+              <h6 id="side_bar_headings">Table List</h6>
             </Navbar.Brand>
           </Navbar>
           <Navbar id="nav_bar">
             <Navbar.Brand href="#home" id="nav_bar">
-              Typography
+              <FontAwesomeIcon
+                icon={faDrumstickBite}
+                style={{ cursor: "pointer" }}
+              />
+              &nbsp;&nbsp;&nbsp;
+              <h6 id="side_bar_headings">Typography</h6>
             </Navbar.Brand>
           </Navbar>
           <Navbar id="nav_bar">
             <Navbar.Brand href="#home" id="nav_bar">
-              Icons
+              <FontAwesomeIcon
+                icon={faNewspaper}
+                style={{ cursor: "pointer" }}
+              />
+              &nbsp;&nbsp;&nbsp;
+              <h6 id="side_bar_headings">Icons</h6>
             </Navbar.Brand>
           </Navbar>
           <Navbar id="nav_bar">
             <Navbar.Brand href="#home" id="nav_bar">
-              Maps
+              <FontAwesomeIcon
+                icon={faMapMarkerAlt}
+                style={{ cursor: "pointer" }}
+              />
+              &nbsp;&nbsp;&nbsp;
+              <h6 id="side_bar_headings">Maps</h6>
             </Navbar.Brand>
           </Navbar>
           <Navbar id="nav_bar">
             <Navbar.Brand href="#home" id="nav_bar">
-              Notifications
+              <FontAwesomeIcon icon={faBell} style={{ cursor: "pointer" }} />
+              &nbsp;&nbsp;&nbsp;
+              <h6 id="side_bar_headings">Notifications</h6>
             </Navbar.Brand>
           </Navbar>
           <Navbar id="nav_bar">
             <Navbar.Brand href="#home" id="nav_bar">
-              RTL Support
+              <FontAwesomeIcon
+                icon={faSnowboarding}
+                style={{ cursor: "pointer" }}
+              />
+              &nbsp;&nbsp;&nbsp;
+              <h6 id="side_bar_headings">RTL Support</h6>
             </Navbar.Brand>
           </Navbar>
           <Navbar id="nav_bar">
             <Navbar.Brand href="#home" id="pro_verson">
-              Upgrade To Pro
+              <FontAwesomeIcon
+                icon={faChevronCircleUp}
+                style={{ cursor: "pointer" }}
+              />
+              &nbsp;&nbsp;&nbsp;
+              <h6 id="side_bar_headings">Upgrade To Pro</h6>
             </Navbar.Brand>
           </Navbar>
         </div>

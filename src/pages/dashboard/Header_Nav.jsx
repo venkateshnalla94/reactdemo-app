5c42463cb956e1b6a2dc604e29c3009fbb5a715f
venkateshnalla94 <venkateshnalla94@gmail.com>
2019-09-07 19:39:40 -0400

    Dashboard Navbar fixed

--- src/pages/dashboard/Header_Nav.jsx
+++ src/pages/dashboard/Header_Nav.jsx
@@ -7,45 +7,57 @@ import {
   faBell,
   faUser
 } from "@fortawesome/free-solid-svg-icons";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 const Header_Nav = props => {
   return (
     <div className="row" id="top_heading">
-      <h4 style={{ fontSize: "1.5rem", fontWeight: "100", width: "122px" }}>
-        {props.name}
-      </h4>
-      <input className="searchField" type="text" placeholder="Search" />
-      <div className="col-sm-1">
-        <FontAwesomeIcon
-          id="fontAwesomSearch"
-          icon={faSearch}
-          style={{
-            cursor: "pointer"
-            // width: "36%",
-            // marginBottom: "20%"
-          }}
-        />
-      </div>
-      <div className="col-sm-1">
-        <FontAwesomeIcon
-          id="fontAwesomSearch"
-          icon={faSatelliteDish}
-          style={{ cursor: "pointer" }}
-        />
-      </div>
-      <div className="col-sm-1">
-        <FontAwesomeIcon
-          id="fontAwesomSearch"
-          icon={faBell}
-          style={{ cursor: "pointer" }}
-        />
-      </div>
-      <div className="col-sm-1">
-        <FontAwesomeIcon
-          id="fontAwesomSearch"
-          icon={faUser}
-          style={{ cursor: "pointer" }}
-        />
-      </div>
+      <Navbar expand="lg" variant="light" bg="dark" style={{ width: "100%" }}>
+        <Container>
+          <Navbar.Brand id="navbar_content" href="#">
+            <h4
+              style={{ fontSize: "1.5rem", fontWeight: "100", width: "122px" }}
+            >
+              {props.name}
+            </h4>
+          </Navbar.Brand>
+          <Navbar.Brand id="navbar_content" href="#">
+            <input className="searchField" type="text" placeholder="Search" />
+          </Navbar.Brand>
+          <Navbar.Brand id="navbar_content" href="#">
+            <FontAwesomeIcon
+              id="fontAwesomSearch"
+              icon={faSearch}
+              style={{
+                cursor: "pointer"
+                // width: "36%",
+                // marginBottom: "20%"
+              }}
+            />
+          </Navbar.Brand>
+          <Navbar.Brand id="navbar_content" href="#">
+            <FontAwesomeIcon
+              id="fontAwesomSearch"
+              icon={faSatelliteDish}
+              style={{ cursor: "pointer" }}
+            />
+          </Navbar.Brand>
+          <Navbar.Brand id="navbar_content" href="#">
+            <FontAwesomeIcon
+              id="fontAwesomSearch"
+              icon={faBell}
+              style={{ cursor: "pointer" }}
+            />
+          </Navbar.Brand>
+          <Navbar.Brand id="navbar_content" href="#">
+            <FontAwesomeIcon
+              id="fontAwesomSearch"
+              icon={faUser}
+              style={{ cursor: "pointer" }}
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
     </div>
   );
 };
